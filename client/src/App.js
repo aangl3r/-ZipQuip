b2decd6a9925702f85aa4b839fb0c94090bee167
aangl3r <justinmchastain@gmail.com>
2019-07-19 17:01:43 -0400

    adding UI diagrams and edits to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import { createMuiTheme } from "@material-ui/core/styles";
 
