e8d4f73869b37150a88d80afd48fa562c8b9c740
aangl3r <justinmchastain@gmail.com>
2019-07-20 02:45:17 -0400

    style Nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -11,7 +11,9 @@ import SearchIcon from '@material-ui/icons/Search';
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
+    background: "#69c9c9"
   },
+
   menuButton: {
     marginRight: theme.spacing(2),
   },
@@ -66,7 +68,7 @@ const Nav = (props) => {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.root}>
         <Toolbar>
           <IconButton
             edge="start"
